c7a6d86c8ac24f0c2482a9bf04c48f754b2e2971
fullwehrer <109440414+fullwehrer@users.noreply.github.com>
2022-12-27 17:09:24 +0100

    removed session ID from link

--- Terminvergabe_Stadt/main.py
+++ Terminvergabe_Stadt/main.py
@@ -14,7 +14,7 @@ voll = "Leider sind aktuell keine freien Termine verfügbar, bitte versuchen Sie
 keineTermineText = voll
 while keineTermineText == voll:
     # driver.get("https://stadt-karlsruhe.saas.smartcjm.com/m/stadt-karlsruhe/extern/calendar/?uid=54f4114e-d167-437b-a0d6-594406f7a0ac")
-    driver.get("https://stadt-karlsruhe.saas.smartcjm.com/m/stadt-karlsruhe-standesamt/extern/calendar/?uid=6385cdf9-0825-4d94-bff2-d4f1315f05f1&wsid=7c282f62-38cf-4b08-9694-6004583cd7b7&lang=de")
+    driver.get("https://stadt-karlsruhe.saas.smartcjm.com/m/stadt-karlsruhe-standesamt/extern/calendar/?uid=6385cdf9-0825-4d94-bff2-d4f1315f05f1&lang=de")
     # assert "Smart" in driver.title
 
     add_button = driver.find_element("xpath","/html/body/div[2]/div[2]/form/div[3]/div[3]/div/div[1]/div/div/div[2]/span[3]")
